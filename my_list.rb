790409188717987c2b807ce7f7547a067cbae5b5
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-12-17 22:31:32 +0200

    Create module MyEnumerable

--- my_list.rb
+++ my_list.rb
@@ -6,3 +6,9 @@ class MyList
     @list = arr
   end
 end
+
+list = MyList.new(1, 2, 3, 4)
+puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+print(list.filter(&:even?))
